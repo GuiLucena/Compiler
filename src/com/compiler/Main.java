fa0d050e1d44b2353b1200a2ff22d3d0b10c151f
Guilherme lucena <lucena9682@gmail.com>
2015-09-17 17:00:51 -0300

    refactored main

--- src/com/compiler/Main.java
+++ src/com/compiler/Main.java
@@ -1,5 +1,4 @@
 package com.compiler;
-
 import com.compiler.scanner.InvalidTokenException;
 import com.compiler.scanner.Scanner;
 import com.compiler.scanner.Token;
@@ -8,23 +7,24 @@ import com.compiler.util.FileReader;
 
 import java.io.IOException;
 
+
 public class Main {
 
-    public static void main(String[] args) throws IOException, InvalidTokenException {
+    public static void main(String[] args) throws IOException {
         FileReader fileReader = new FileReader();
-        Token token = null;
-        fileReader.open(args[0]);
-        Scanner scanner = new Scanner(fileReader);
         try {
+            fileReader.open(args[0]);
+            Token token;
+            Scanner scanner = new Scanner(fileReader);
             do {
                 token = scanner.getNextToken();
-                if (token != null)
-                    System.out.println(token.toString());
             } while (token.getClassfication() != TokenClassification.EOF);
+            System.out.println("scan completed");
         }catch (InvalidTokenException ex){
             System.out.println(ex.getMessage());
-        }
-        catch (IOException ex){
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }catch (Exception ex){
             System.out.println(ex.getMessage());
         }
         finally {
